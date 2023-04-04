376aba93c3023b08b101a6be67fc0d945cd9201e
Rolly Maulana Awangga <rolly@awang.ga>
2023-04-04 11:25:37 +0700

    tugas

--- bella_test.go
+++ bella_test.go
@@ -7,40 +7,40 @@ import (
 
 
 func TestInsertMataKuliah(t *testing.T) {
-	namamatakuliah := "Pemrograman III"
-	kodematakuliah := "TI41264"
-	dosen := "ROLLY MAULANA AWANGGA"
-	sks := "3"
+	namamatakuliah := "Literasi Manusia"
+	kodematakuliah := "TI41278"
+	dosen := "NOVIANA RIZA"
+	sks := "2"
 	hasil := InsertMataKuliah(namamatakuliah, kodematakuliah, dosen, sks)
 	fmt.Println(hasil)
 }
 
 func TestInsertJadwalKuliah(t *testing.T) {
-	hari := "Senin"
-	jammulai := "13:00"
-	jamselesai := "18:00"
-	ruang := "201"
+	hari := "Rabu"
+	jammulai := "10:00"
+	jamselesai := "12:00"
+	ruang := "310"
 	hasil := InsertJadwalKuliah(hari, jammulai, jamselesai, ruang)
 	fmt.Println(hasil)
 }
 
 func TestInsertKelas(t *testing.T) {
-	ruang := "201"
-	kapasitasmhs := "40"
+	ruang := "310"
+	kapasitasmhs := "25"
 	hasil := InsertKelas(ruang, kapasitasmhs)
 	fmt.Println(hasil)
 }
 
 func TestInsertDosen(t *testing.T) {
-	namadosen := "ROLLY MAULANA AWANGGA"
-	kodedosen := "TI41264"
-	matakuliah := "Pemrograman III"
+	namadosen := "NOVIANA RIZA"
+	kodedosen := "TI41278"
+	matakuliah := "Literasi Manusia"
 	hasil := InsertDosen(namadosen, kodedosen, matakuliah)
 	fmt.Println(hasil)
 }
 
 func TestInsertMahasiswa(t *testing.T) {
-	namamhs := "GABRIELLA YOUZANNA RORONG"
+	namamhs := "ARDIVA PUTRI TAVA PRAMESWARI"
 	kelas := "2A"
 	prodi := "D4 Teknik Informatika"
 	hasil := InsertMahasiswa(namamhs, kelas, prodi)
